3b8a03b974e39b5505669c3fe70284ee038a6279
Shandy Sulen <shandysulen@gmail.com>
2024-02-22 20:15:17 -0500

    uppercase

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Index() {
       size='xl'
       className='flex flex-col justify-center items-center min-h-[calc(100vh-var(--height-header)-var(--height-footer))] h-full gap-4'
     >
-      <Heading>Todo app</Heading>
+      <Heading>Todo App</Heading>
       <Content />
     </Container>
   );
